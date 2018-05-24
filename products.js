06cdc8ba1322a3ee2cc26878d93a6e7e46291954
enc0d3rw <enc0d3rw@gmail.com>
2018-05-24 15:19:47 +0300

    Чистый проект

---
+++ products.js
@@ -0,0 +1,8 @@
+// Массив товаров
+var products = [
+    {id: 0, name: 'product1', description: 'description01', image: 'photo.jpg', price: 1000},
+    {id: 1, name: 'product2', description: 'description02', image: 'product-photo.png', price: 1300}
+];
+
+var basketModule = require('./Basket');
+var Basket = new basketModule();
